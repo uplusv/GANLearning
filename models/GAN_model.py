44364b2818d3e7d3484efebbb1e4fe7763bf7373
uplusv <uplusv@163.com>
2022-07-28 18:15:43 +0800

    add register and connect  with arch

--- models/GAN_model.py
+++ models/GAN_model.py
@@ -1,9 +1,15 @@
+from atexit import register
 import torch
 from basic_model import BaseModel
+from DCNGAN_arch import DCNGAN
+from utils.register import MODEL_REGISTRY
 
+
+@MODEL_REGISTRY.register()
 class GAN(BaseModel):
     def __init__(self, opts):
         super().__init__(opts)
+        self.model = DCNGAN()
 
     def feed_data(self, data):
         return super().feed_data(data)
